82810dee1e59b62c9dc934c735e4ac1c155a16c4
Sandro Baumgartner <bausac16@htl-kaindorf.ac.at>
2019-03-05 09:59:59 +0100

    implemented label and enum

--- Exercise_401_Uhrzeit/src/bl/UhrzeitLabel.java
+++ Exercise_401_Uhrzeit/src/bl/UhrzeitLabel.java
@@ -1,11 +1,17 @@
 package bl;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 public class UhrzeitLabel extends JLabel {
+    private Ziffern ziffer;
 
-    public UhrzeitLabel() {
-        System.out.println("asdf");
+    public UhrzeitLabel(Ziffern ziffer) {
+        this.ziffer = ziffer;
     }
-    
+
+    public void setZiffer(Ziffern ziffer) {
+        this.ziffer = ziffer;
+        this.setIcon(new ImageIcon(getClass().getResource("/icons/"+ziffer.getZiffer()+".png")));
+    }
 }
---
+++ Exercise_401_Uhrzeit/src/bl/Ziffern.java
@@ -0,0 +1,15 @@
+package bl;
+
+public enum Ziffern {
+    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9);
+    
+    int ziffer;
+
+    private Ziffern(int ziffer) {
+        this.ziffer = ziffer;
+    }
+
+    public int getZiffer() {
+        return ziffer;
+    }
+}
